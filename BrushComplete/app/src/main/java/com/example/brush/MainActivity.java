7f026c06b9580aeb6cd8c17f9a05ce950e74fc5d
sadractijerina <reavity3@gmail.com>
2019-05-02 09:14:47 -0500

    project

--- BrushComplete/app/src/main/java/com/example/brush/MainActivity.java
+++ BrushComplete/app/src/main/java/com/example/brush/MainActivity.java
@@ -103,6 +103,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
         userRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
